1586c888567e9f0a38c38182b6cd91902bdac9ec
suvam-das-29 <suvam.das.another@gmail.com>
2025-07-27 23:19:11 +0530

    Added UserPanel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Components/Home';
 import AdminPanel from './Admin/AdminPanel';
+import UserPanel from './User/UserPanel';
 import NotFound from './NotFound';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css'
@@ -14,6 +15,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home />}></Route>
+          <Route path='/user' element={<UserPanel />}></Route>
           <Route path='/admin' element={<AdminPanel />}></Route>
           <Route path='*' element={<NotFound />}></Route>
         </Routes>
---
+++ src/User/UserPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserPanel = () => {
+  return (
+    <>
+        <h1>Hello user panel</h1>
+    </>
+  )
+}
+
+export default UserPanel
